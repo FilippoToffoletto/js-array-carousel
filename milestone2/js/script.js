423c55be03ef43e353e2a14fe213690c1bae7633
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-17 16:37:03 +0200

    [js]add imgs array

---
+++ milestone2/js/script.js
@@ -0,0 +1,11 @@
+/* Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript. */
+
+const imagesArray = [
+    '../img/01.jpg',
+    '../img/02.jpg',
+    '../img/03.jpg',
+    '../img/04.jpg',
+    '../img/05.jpg',
+];
