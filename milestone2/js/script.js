52b8f9ee13edbef5ee569d1f2837343c254cd889
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-17 17:03:59 +0200

    [js]add dynamic array and active img

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -2,6 +2,8 @@
 Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript. */
 
+
+// inserisco la banca dati che include le immagini
 const imagesArray = [
     '../img/01.jpg',
     '../img/02.jpg',
@@ -9,3 +11,28 @@ const imagesArray = [
     '../img/04.jpg',
     '../img/05.jpg',
 ];
+
+// creaiamo una constante vuota iniziale
+let imageTags = '';
+
+// seleziono il contenitore che conterrà i tag img
+const slider = document.querySelector('.item-wrapper');
+
+for(let i = 0; i < imagesArray.length; i++){
+
+    imagesTags = `
+        <img src="${imagesArray[i]}" alt="${imagesArray[i]}" class="item">
+    `;
+    
+}
+
+let counterImages = 0;
+
+// aggiungo i tag generati dinamicamente allo slider
+slider.innerHTML = imagesTags;
+
+// salvo tutti gli elementi che hanno classe item in una collection (un array di elementi html)
+const items = document.getElementsByClassName('item');
+
+// prendo il primo elemento della collection e aggiungo la classe active
+items[counterImages].classList.add('active');
